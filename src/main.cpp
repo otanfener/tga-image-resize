ab1fff9c866ed34f8220da4f751e73c6bac02615
Ozan Tanfener <ozan.tanfener@gmail.com>
2021-05-04 23:21:47 +0200

    Information output

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,28 @@ int main(int argc, const char *argv[]) {
     try {
         //Decoder
         TgaImage originalImage;
+        std::cout << "Reading image file: " << fileName << std::endl;
         Decoder decoder(originalImage);
-        auto imageBuffer = decoder.Decode(fileName);
 
+        auto imageBuffer = decoder.Decode(fileName);
+        std::cout << "Resizing an image with dimensions: " << originalImage.tgaHeader_.width << "x"
+                  << originalImage.tgaHeader_.height << std::endl;
         //Interpolation
         auto processedImageBuffer = BiLinearInterPolation(imageBuffer, originalImage);
+        std::cout << "Resizing completed, new dimensions: " << originalImage.tgaHeader_.width / 2 << "x"
+                  << originalImage.tgaHeader_.height / 2 << std::endl;
 
         //Encoder
         TgaImage newImage;
         std::string fileNameWithoutExtension = fileName.substr(0, fileName.find(STRING_DELIMITER));
         std::string newFileName = fileNameWithoutExtension + PROCESSED_FILENAME_POSTFIX;
         Encoder encoder(newImage);
+
+        std::cout << "Creating a new image: " << newFileName << std::endl;
         encoder.Encode(newFileName, processedImageBuffer, originalImage.tgaHeader_.width / 2,
                        originalImage.tgaHeader_.height / 2, originalImage.tgaHeader_.bits);
 
-
+        std::cout << "Operation completed" << std::endl;
         return static_cast<int>(StatusCode::SUCCESS);
     } catch (const TgaException &e) {
         std::cerr << "TgaException catched : \n" << e.what();
